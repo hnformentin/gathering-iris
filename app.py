e3f42831521c996e7e6bc2964269c22bec660820
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2020-10-30 09:00:15 +0100

    hello/world api endpoint

--- app.py
+++ app.py
@@ -8,5 +8,11 @@ app = Flask("name-gathering")
 def root():
     return render_template("index.html")
 
+
+@app.route("/hello")
+def hello():
+    return {"hello": "world"}
+
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
