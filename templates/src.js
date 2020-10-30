25e9a626986329a537307beb5ab632689c0d117d
Jens Gåsemyr Magnus <jegm@equinor.com>
2020-10-30 09:53:34 +0100

    Post user file

--- templates/src.js
+++ templates/src.js
@@ -36,6 +36,32 @@ function geoFindMe() {
 
 geoFindMe()
 
+const url = "/uploadUserData";
+const form = document.querySelector('#userFileForm');
+
+// add event listener
+form.addEventListener('submit', e => {
+
+    // disable default action
+    e.preventDefault();
+
+    // collect files
+    const files = document.querySelector('[name=file]').files;
+    const formData = new FormData();
+    formData.append('userData', files[0]);
+
+    // post form data
+    const xhr = new XMLHttpRequest();
+
+    // log response
+    xhr.onload = () => {
+        console.log(xhr.responseText);
+    };
+
+    // create and send the reqeust
+    xhr.open('POST', url);
+    xhr.send(formData);
+});
 
 data = [
     {x: 0, y: 0},
